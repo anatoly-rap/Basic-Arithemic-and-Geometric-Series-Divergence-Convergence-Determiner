5f2787583735fde5b36bd03146bac1e038b08e28
Anatoly <102273751+anatoly-rap@users.noreply.github.com>
2022-03-27 19:42:53 -0600

    cleaning up args
    restructured methods and args.

--- app.py
+++ app.py
@@ -6,78 +6,81 @@ from decimal import Decimal
 from fractions import Fraction
 import numpy as np
 
-ab = False
 gb = False
+ab = False 
 
-def menu():
-    
+def Menu():
     print("Hello, welcome to the sequence Divergence/Convergence Determiner")
     print("What type of sequence will you be looking at today?")
     print("[a]Arithmetic")
     print("[g]Geometric: Expoential, expression rasied to a power") 
+               
+def grabSeqence(answer):
     
-def Arth():
+    if answer == 'g':
+        
+        global gb
+        gb = True
+        print("Geomtric sequences are in form a_n = ar^(n-1)")
+        inputNumbers = list(map(float, input("Please type your numbers here (comma separated): ").split(",")))
+        print("a_1 is: " + str(inputNumbers[0]))
+        print("Common Ratio is: " + str(inputNumbers[1]))
+        return inputNumbers
     
-    print("Artithmic sequences are in form of a_n = a_1 + d(n-1) ")
-    a_1 = input("a_1 is the first term in the sequance, which is ")
-    d = input("d is the common difference, which is ")
-    global ab
-    ab = True
-    return a_1, d
-            
-def Geometric():
+    elif answer == 'a':
+        
+        global ab
+        ab = True
+        print("Artithmic sequences are in form of a_n = a_1 + d(n-1) ")
+        inputNumbers = list(map(float, input("Please type your numbers here (comma separated): ").split(", ")))
+                
+        print("a_1 is: " + str(inputNumbers[0]))
+        print("Common difference is: " + str(inputNumbers[1]))
+        return inputNumbers
     
-    print("Geomtric sequences are in form a_n = ar^(n-1)")
-    var_a = input("what is the starting value of the sequence?[value of a)? ")
-    var_r = input("what is the common ratio of your geometric sequence? ")
-    global gb
-    gb = True
-    return var_a, var_r
-   
 def getSeq():
-    
-    menu()
-    answer = input()
+    Menu()
+    answer = input("[g]eometric or [a]rithemtic?")
     if answer == 'a':
-        Arth()
+        return answer
     elif answer == "g":
-        Geometric()
+        return answer
     else:
         print("Error- input does not match any of the listed options ")
             
-def getL():
-    
+def Determiner(var1,var2):
     x = []  
     y = []
-    fig, ax = plt.subplots()
-    
     if gb is True:
+        fig, ax = plt.subplots()
         i = 1
-        sum = 0 
-        var_a, var_r = Geometric()
-        for i in range(0, i+25):
-            sum = sum + (int(var_a)* float(var_r))**(i- 1)
+        sum = 0
+        for i in range(1, i+24):
+            sum = sum + ((var1)*(var2))**(i- 1)
             x.append(i)
             y.append(sum)
             
-        plt.scatter(x,y, s = 1.5)
-        plt.title('$\sum_{i=1}^\infty' + str(var_a) + '\cdot('+ str(Fraction(var_r)) +')^{i-1} $')
+        ax.axis([0, 25, 0, 10])
+        ax.scatter(x,y, s = 1.5)
+        plt.title('$\sum_{i=1}^\infty' + str(var1) + '\cdot'+ str(Fraction(var2)) +'^{i-1} $')
         
-        if abs(float(var_r)) < 1:
-            ax.text(0.3,0.1,'Since chosen r:  |' + str(var_r) + '| < 1.\n The sequence converges to ' + str(sum)[0:4] +  ' for the first '+ str(np.amax(x)) +' terms',
+        if abs((var2)) < 1:
+            ax.text(0.3,0.1,'Since chosen r,  |' + str(var2) + '| < 1:\n The sequence converges to ' + str(sum)[0:4] +  ' for the first '+ str(np.amax(x)) +' terms',
                     transform=ax.transAxes, fontsize=9, verticalalignment='bottom', horizontalalignment= 'center')
+            ax.annotate("sum conversion", xy=(0.3,0.1), xytext=(0.3, 0.1), arrowprops={"arrowstyle":"->", "color":"black"})
             plt.show()                                                                                                      
-        elif abs(float(var_r)) >= 1:
-               ax.text(0.3,0.1,'Since chosen r: 1 ≤ |' + str(var_r) + '|. \n The sequence diverges to ' + str(sum)[0:4] +
-                    ' for the first '+str(np.amax(x)) +' terms',
-                    fontsize=9, transform=ax.transAxes, horizontalalignment='center',verticalalignment='bottom')
-        plt.show()
+        elif abs((var2)) >= 1:
+               ax.text(0.3,0.1,'Since chosen r, 1 ≤ |' + str(var2) + '|:\n The sequence diverges to ' + str(sum)[0:4] +
+                    ' for the first '+ str(np.amax(x)) +' terms',transform=ax.transAxes, fontsize=9, horizontalalignment='center',verticalalignment='bottom')
+        plt.show() 
+         
+    else:
+        print("This diverges to infinity. All arithmatic sequences diverge")
         
-    elif ab is True:
-        print("this diverges to Infinity. All arithmatic sequences diverge")
                  
 def main():
-    getSeq()
-    getL()
+    user_input = getSeq()
+    Determiner(*grabSeqence(user_input))
+    
 if __name__ == '__main__':
     main()
