bceb04297f3814b8fbd635f7dfad563a8d66ea5c
Ana -rr <102273751+anatoly-rap@users.noreply.github.com>
2023-02-19 19:18:44 -0700

    Optimization
    removed redundant variables and used list comprehension in situ of iterative for loop

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from fractions import Fraction
 import numpy as np
 
 gb = False
-ab = False 
+ab = False
 
 def Menu():
     print("Hello, welcome to the sequence Divergence/Convergence Determiner")
@@ -19,11 +19,8 @@ def Menu():
     print("   ___")
     print("  n = 0")
     print("What type of sequence will you be looking at today?")
-       
 def grabSeqence(answer):
-    
     if answer == 'g':
-        
         global gb
         gb = True
         print("Geomtric sequences are in form a_n = ar^(n-1)")
@@ -31,18 +28,14 @@ def grabSeqence(answer):
         print("a_1 is: " + str(inputNumbers[0]))
         print("Common Ratio is: " + str(inputNumbers[1]))
         return inputNumbers
-    
     elif answer == 'a':
-        
         global ab
         ab = True
         print("Artithmic sequences are in form of a_n = a_1 + d(n-1) ")
         inputNumbers = list(map(float, input("Please type your numbers here (comma separated): ").split(", ")))
-                
         print("a_1 is: " + str(inputNumbers[0]))
         print("Common difference is: " + str(inputNumbers[1]))
         return inputNumbers
-    
 def getSeq():
     Menu()
     answer = input("[g]eometric or [a]rithemtic?")
@@ -51,31 +44,22 @@ def getSeq():
     elif answer == "g":
         return answer
     else:
-        print("Error- input does not match any of the listed options ")
-            
+        print("Error- input does not match any of the listed options ")    
 def Determiner(var1,var2):
-    x = []  
-    y = []
     if gb is True:
         fig, ax = plt.subplots()
-        i = 1
         sum = 0
-        for i in range(1, i+29):
-            sum = sum + ((var1)*(var2))**(i- 1)
-            x.append(i)
-            y.append(sum)
-            
+        for i in range(1, 30):
+            sum += math.pow((var1*var2), i-1)
+            x = [i for i in range(1, 30)]
+            y = [sum(y[:i]) for i in range(1, 30)]
         ax.axis([0, 30, 0, max(y)+ 1])
         ax.scatter(x,y, s = 1.8, color = "red", label = "a_n sequence")
-        
         plt.title('$\sum_{i=1}^\infty' + str(var1) + '\cdot'+ str(Fraction(var2).limit_denominator(10)) +'^{i-1}$')
         plt.axhline(y = sum, color = 'g', alpha = 0.8, linestyle = '-', label = "limit")
-        
         ax.fill_between(x, sum - sum*0.08, sum + sum*0.08, facecolor='plum',alpha=0.3)
         ax.legend(loc='lower right')
-        
         if abs((var2)) < 1:
-            
             ax.text(0.9,0.9,'Since chosen r, |' + str(var2) + '| < 1:\n The sequence converges to ' + str(sum)[0:4] +' for the first '+ str(np.amax(x)) +' terms', 
                     verticalalignment='top',
                     horizontalalignment='right',
@@ -86,25 +70,19 @@ def Determiner(var1,var2):
                         xy=(max(x),max(y)), xytext=(max(x) - .5, max(y) + .5),
                         fontsize= 7, 
                         arrowprops={"arrowstyle":"->", "color":"y"})
-            plt.show()    
-                                                                                                              
+            plt.show()                                                                                              
         elif abs((var2)) >= 1:
-            
                ax.text(0.3,0.1,'Since chosen r, 1 ≤ |' + str(var2) + '|:\n The sequence diverges to ' + str(sum)[0:4] +' for the first '+ str(np.amax(x)) +' terms',
                     horizontalalignment='right',
                     verticalalignment='top',
                     fontsize=7,
                     bbox={'facecolor':'white', 'alpha':0.6, 'pad':10},
                     transform= ax.transAxes)
-        plt.show() 
-         
+        plt.show();
     else:
         print("This diverges to infinity. All arithmatic sequences diverge")
-        
-                 
 def main():
     user_input = getSeq()
     Determiner(*grabSeqence(user_input))
-    
 if __name__ == '__main__':
     main()
